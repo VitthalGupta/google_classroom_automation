f2042bde88571c4e965b71e13154c49d4eef4de6
Vitthal Gupta <vitthalgupta27@gmail.com>
2020-12-22 12:12:48 +0530

    Update the time
    Changes the time to 1 hour

--- classes.py
+++ classes.py
@@ -101,7 +101,8 @@ def join_class(class_name):
 	time.sleep(1)
 	join=driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div/div[8]/div[3]/div/div/div[2]/div/div[1]/div[2]/div/div[2]/div/div[1]/div[1]/span/span')
 	join.click()
-	time.sleep(15)
+	# Change the duration of the class according to your need (in seconds)
+	time.sleep(3600)
 	driver.quit()
 
 #Variable for the classes x-path
